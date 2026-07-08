d7a12a4474e398bc309c30c54d51f219f58f12d6
SiliasOdion809 <siliasodion809@gmail.com>
2026-07-08 12:50:02 +0100

    refactor(iam): replace AWS managed Lambda logging policy with customer-managed policy

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -18,12 +18,6 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_basic" {
-  role = aws_iam_role.lambda_role.name
-
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
 resource "aws_iam_user" "bedrock_dev_view" {
   name = "bedrock-dev-view"
 
