89ff33a3aac7fe529ce8cd98c0385db9b4fdd454
SiliasOdion809 <siliasodion809@gmail.com>
2026-07-13 16:44:13 +0100

    feat(ci): migrate Terraform workflow to GitHub OIDC

--- terraform/modules/iam/github_actions.tf
+++ terraform/modules/iam/github_actions.tf
@@ -64,8 +64,7 @@ data "aws_iam_policy_document" "github_deployment_permissions" {
     actions = [
       "s3:GetObject",
       "s3:PutObject",
-      "s3:DeleteObject",
-      "s3:ListBucket"
+      "s3:DeleteObject"
     ]
 
     resources = [
