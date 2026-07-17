2fa7a15edda23856b14320b3b4ffb229eaf3d8b8
SiliasOdion809 <siliasodion809@gmail.com>
2026-07-17 16:17:52 +0100

    fix(terraform): reconcile infrastructure state after configuration drift

--- terraform/modules/iam/github_terraform_policy.tf
+++ terraform/modules/iam/github_terraform_policy.tf
@@ -60,7 +60,8 @@ data "aws_iam_policy_document" "github_terraform_permissions" {
       "s3:PutObject",
       "s3:DeleteObject",
       "s3:GetBucketAcl",
-      "s3:GetBucketWebsite"
+      "s3:GetBucketWebsite",
+      "s3:GetAccelerateConfiguration"
     ]
 
     resources = ["*"]
